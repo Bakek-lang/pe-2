084cfbdc0a9d1980b9d403a134a8f1ec8acd9f1e
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2025-01-23 21:28:10 +0100

    venue homepage setup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,17 @@
+import { useEffect, useState } from "react";
+
 export default function Home() {
-  return <div>HOME</div>;
+  const [venues, setVenues] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch("https://v2.api.noroff.dev/holidaze/venues");
+      const data = await response.json();
+      console.log("data", data);
+      setVenues(data.data);
+    }
+    getData();
+  }, []);
+
+  return <></>;
 }
