708daac223a397dee0d335d740ffcec1215ee56a
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2025-01-23 21:55:26 +0100

    Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
+import VenuePage from "./pages/VenuePage";
+import PageNotFound from "./pages/PageNotFound";
 
 export default function App() {
   return (
@@ -8,6 +10,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="/venues/:id" element={<VenuePage />} />
+          <Route path="*" element={<PageNotFound />} />
         </Route>
       </Routes>
     </>
