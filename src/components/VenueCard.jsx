74bf22c5bbfc948bf366b75c347c550829d45059
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2025-01-24 22:24:50 +0100

    content added

--- src/components/VenueCard.jsx
+++ src/components/VenueCard.jsx
@@ -1,9 +1,20 @@
 export default function VenueCard({ venue }) {
   return (
-    <div className="rounded-lg shadow-lg max-w-sm  ">
-      <img src="/" alt="/" className="w-48 h-full object-cover" />
-      <div className="">
-        <h2>{venue.name}</h2>
+    <div className="rounded-lg shadow-lg max-w-sm m-4 pb-4 flex flex-col">
+      <img
+        src={venue.media[0].url}
+        alt="/"
+        className="w-48 h-full object-cover rounded-t-lg"
+      />
+      <div className="p-2">
+        <p>
+          {venue.location.city}, {venue.location.country}
+        </p>
+        <div className="flex justify-between ">
+          <h2>{venue.name}</h2>
+          <span>{venue.rating}</span>
+        </div>
+        <p>{venue.description}</p>
       </div>
     </div>
   );
