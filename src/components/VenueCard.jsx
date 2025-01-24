7458dfdda7504ab7c8ff702474efb6468c4313fd
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2025-01-24 16:03:06 +0100

    Styling

--- src/components/VenueCard.jsx
+++ src/components/VenueCard.jsx
@@ -1,7 +1,8 @@
 export default function VenueCard({ venue }) {
   return (
-    <div>
-      <div className="mb-4">
+    <div className="rounded-lg shadow-lg max-w-sm  ">
+      <img src="/" alt="/" className="w-48 h-full object-cover" />
+      <div className="">
         <h2>{venue.name}</h2>
       </div>
     </div>
