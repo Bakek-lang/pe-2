1fb26ca5c1735b4070b8ecb459f97c2b24b7b44f
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2025-01-24 22:35:47 +0100

    meta add

--- src/components/VenueCard.jsx
+++ src/components/VenueCard.jsx
@@ -7,14 +7,20 @@ export default function VenueCard({ venue }) {
         className="w-48 h-full object-cover rounded-t-lg"
       />
       <div className="p-2">
-        <p>
+        <p className=" text-gray-500">
           {venue.location.city}, {venue.location.country}
         </p>
         <div className="flex justify-between ">
-          <h2>{venue.name}</h2>
+          <h2 className="text-xl">{venue.name}</h2>
           <span>{venue.rating}</span>
         </div>
         <p>{venue.description}</p>
+        <p>Max Guests: {venue.maxGuests}</p>
+        <p>{venue.price} NOK per night</p>
+        {venue.meta.wifi && <p>Wifi</p>}
+        {venue.meta.breakfast && <p>Breakfast</p>}
+        {venue.meta.parking && <p>Parking</p>}
+        {venue.meta.pets && <p>Pets</p>}
       </div>
     </div>
   );
