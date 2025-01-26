39a0f489c6dfcbef46ada30a949166ef099675a5
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2025-01-26 17:24:22 +0100

    filter on venues with images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,16 +3,20 @@ import VenueCard from "../components/VenueCard";
 
 export default function Home() {
   const [venues, setVenues] = useState([]);
-  /*   const [page, setPage] = useState(1); */
+  const [page, setPage] = useState(1);
 
-  /* async function fetchVenues(pageNumber) {
+  async function fetchVenues(pageNumber) {
     const response = await fetch(
       `https://v2.api.noroff.dev/holidaze/venues?limit=30&page=${pageNumber}`
     );
     const data = await response.json();
     console.log("data", data);
-    setVenues((prev) => [...prev, ...data.data]);
-    setVenues(data.data)
+
+    const venuesWithImages = data.data.filter(
+      (venue) => venue.media && venue.media.length > 0
+    );
+
+    setVenues((prev) => [...prev, ...venuesWithImages]);
     console.log("venues", venues);
   }
 
@@ -22,11 +26,13 @@ export default function Home() {
 
   function handleViewMore() {
     setPage((prevPage) => prevPage + 1);
-  } */
+  }
 
-  useEffect(() => {
+  /* useEffect(() => {
     async function getData() {
-      const response = await fetch("https://v2.api.noroff.dev/holidaze/venues");
+      const response = await fetch(
+        "https://v2.api.noroff.dev/holidaze/venues?limit=30"
+      );
       const data = await response.json();
       console.log("data", data);
       setVenues(data.data);
@@ -34,12 +40,16 @@ export default function Home() {
     getData();
   }, []);
 
+  console.log("all venues", venues);
   const firstVenues = venues.slice(0, 20);
+  console.log("first 20 venues: ", firstVenues); */
+
+  console.log("all venues", venues);
 
   return (
     <div>
       <div className="flex flex-wrap justify-center gap-4 p-4">
-        {firstVenues.map((venue) => (
+        {venues.map((venue) => (
           <VenueCard venue={venue} key={venue.id} />
         ))}
       </div>
