5c848aba29b2a4898b7420ec7dc9d4ba9ca5e158
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2025-01-26 21:23:31 +0100

    venue page setup

--- src/pages/VenuePage.jsx
+++ src/pages/VenuePage.jsx
@@ -4,25 +4,42 @@ import { fetchVenueById } from "../js/utils/fetchVenue";
 
 export default function VenuePage() {
   const [venue, setVenue] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
   let { id } = useParams();
 
   useEffect(() => {
     async function loadVenue() {
       try {
+        setIsLoading(true);
+        setIsError(false);
         const venueData = await fetchVenueById(id);
         console.log("This is the venue data: ", venueData);
-        setVenue(venueData);
+        setVenue(venueData.data);
       } catch (error) {
         console.log("Error getting venue data: ", error);
       }
+      setIsLoading(false);
     }
 
     loadVenue();
   }, [id]);
 
+  if (isLoading || !venue) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error</div>;
+  }
+
+  if (!venue) {
+    return <div>Venue not found.</div>;
+  }
+
   return (
     <div>
-      VENUE PAGE with id: {id}, and venue is: {venue.title}
+      VENUE PAGE with id: {id}, and venue is: {venue.name}
     </div>
   );
 }
