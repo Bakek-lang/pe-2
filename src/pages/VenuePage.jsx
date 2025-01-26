f253670f3c1268972dea294ffd63b74381fc8c70
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2025-01-26 18:55:19 +0100

    venue page

--- src/pages/VenuePage.jsx
+++ src/pages/VenuePage.jsx
@@ -1,3 +1,28 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { fetchVenueById } from "../js/utils/fetchVenue";
+
 export default function VenuePage() {
-  return <div>VENUE PAGE</div>;
+  const [venue, setVenue] = useState(null);
+  let { id } = useParams();
+
+  useEffect(() => {
+    async function loadVenue() {
+      try {
+        const venueData = await fetchVenueById(id);
+        console.log("This is the venue data: ", venueData);
+        setVenue(venueData);
+      } catch (error) {
+        console.log("Error getting venue data: ", error);
+      }
+    }
+
+    loadVenue();
+  }, [id]);
+
+  return (
+    <div>
+      VENUE PAGE with id: {id}, and venue is: {venue.title}
+    </div>
+  );
 }
