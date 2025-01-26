d182b915bc2374007d2cb619a7d45fb64da09532
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2025-01-26 18:49:42 +0100

    fetch single venue

---
+++ src/js/utils/fetchVenue.js
@@ -0,0 +1,12 @@
+export async function fetchVenueById(id) {
+  try {
+    const response = await fetch(
+      `https://v2.api.noroff.dev/holidaze/venues/${id}`
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log("Failed to fetch venue details: ", error);
+    throw error;
+  }
+}
