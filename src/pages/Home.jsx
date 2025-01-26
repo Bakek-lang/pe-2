6cee63aaa0c33a002f58a934795277f8048e7e89
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2025-01-26 17:16:29 +0100

    fetch view more

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,26 @@ import VenueCard from "../components/VenueCard";
 
 export default function Home() {
   const [venues, setVenues] = useState([]);
+  /*   const [page, setPage] = useState(1); */
+
+  /* async function fetchVenues(pageNumber) {
+    const response = await fetch(
+      `https://v2.api.noroff.dev/holidaze/venues?limit=30&page=${pageNumber}`
+    );
+    const data = await response.json();
+    console.log("data", data);
+    setVenues((prev) => [...prev, ...data.data]);
+    setVenues(data.data)
+    console.log("venues", venues);
+  }
+
+  useEffect(() => {
+    fetchVenues(page);
+  }, [page]);
+
+  function handleViewMore() {
+    setPage((prevPage) => prevPage + 1);
+  } */
 
   useEffect(() => {
     async function getData() {
