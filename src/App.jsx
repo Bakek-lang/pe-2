e9835d848ad58c75d345ab4036cef4fbd8e788eb
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2025-01-27 15:49:27 +0100

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import VenuePage from "./pages/VenuePage";
 import PageNotFound from "./pages/PageNotFound";
+import RegisterPage from "./pages/RegisterPage";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="/venue/:id" element={<VenuePage />} />
+          <Route path="/register" element={<RegisterPage />} />
           <Route path="*" element={<PageNotFound />} />
         </Route>
       </Routes>
