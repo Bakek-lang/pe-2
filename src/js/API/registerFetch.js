a81f8daa4744af5aca77af813e2437ee3029bec2
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2025-01-27 18:27:53 +0100

    error handling

--- src/js/API/registerFetch.js
+++ src/js/API/registerFetch.js
@@ -1,3 +1,5 @@
+import { API_AUTH, API_BASE, API_REGISTER } from "./constants";
+
 export async function registerUser(name, email, password) {
   const response = await fetch(API_BASE + API_AUTH + API_REGISTER, {
     headers: {
@@ -8,6 +10,9 @@ export async function registerUser(name, email, password) {
   });
 
   if (!response.ok) {
-    return response.json();
+    const errorData = await response.json();
+    throw new Error(errorData.message || "Registration failed");
   }
+
+  return response.json();
 }
