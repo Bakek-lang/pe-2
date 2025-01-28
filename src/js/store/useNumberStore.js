d99c7d1e69971e84770ac78950631b5170006814
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2025-01-28 22:50:08 +0100

    test persist

--- src/js/store/useNumberStore.js
+++ src/js/store/useNumberStore.js
@@ -1,10 +1,18 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-const useNumberStore = create((set) => ({
-  number: 42,
-  increase: () => set((state) => ({ number: state.number + 1 })),
-  decrease: () => set((state) => ({ number: state.number - 1 })),
-  setNumber: (newNumber) => set({ number: newNumber }),
-}));
+const useNumberStore = create(
+  persist(
+    (set) => ({
+      number: 42,
+      increase: () => set((state) => ({ number: state.number + 1 })),
+      decrease: () => set((state) => ({ number: state.number - 1 })),
+      setNumber: (newNumber) => set({ number: newNumber }),
+    }),
+    {
+      name: "numberStorage",
+    }
+  )
+);
 
 export default useNumberStore;
