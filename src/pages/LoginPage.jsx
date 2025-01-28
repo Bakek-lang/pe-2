cf1bf77c5909c65f0e997ac3993e2ed056b701ae
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2025-01-28 22:46:18 +0100

    test

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,34 +1,46 @@
+import useNumberStore from "../js/store/useNumberStore";
+
 export default function LoginPage() {
+  const { number, increase, decrease, setNumber } = useNumberStore();
+
   return (
-    <div className="flex flex-col items-center">
-      <form className="wax-w-xl py-8 px-4">
-        <div className="mb-4">
-          <label>Email</label>
-          <input
-            type="email"
-            name="email"
-            placeholder="Email"
-            className="border rounded w-full py-2 px-4 border-black"
-          />
-        </div>
+    <div>
+      <div className="flex flex-col items-center mb-52">
+        <h1>Current Number: {number}</h1>
+        <button onClick={increase}>Increase</button>
+        <button onClick={decrease}>Decrease</button>
+        <button onClick={() => setNumber(100)}>Set to 100</button>
+      </div>
+      <div className="flex flex-col items-center">
+        <form className="wax-w-xl py-8 px-4">
+          <div className="mb-4">
+            <label>Email</label>
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              className="border rounded w-full py-2 px-4 border-black"
+            />
+          </div>
 
-        <div className="mb-4">
-          <label>Password</label>
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            className="border rounded w-full py-2 px-4 border-black"
-          />
-        </div>
+          <div className="mb-4">
+            <label>Password</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              className="border rounded w-full py-2 px-4 border-black"
+            />
+          </div>
 
-        <button
-          className="bg-blue-500 rounded-lg py-2 px-4 mt-2 text-white  "
-          type="submit"
-        >
-          Login
-        </button>
-      </form>
+          <button
+            className="bg-blue-500 rounded-lg py-2 px-4 mt-2 text-white  "
+            type="submit"
+          >
+            Login
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
