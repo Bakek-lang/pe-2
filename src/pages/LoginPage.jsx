c3b0640bc67e69c9c24a02f49e41c2df510e4779
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2025-01-28 22:25:25 +0100

    setup

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,3 +1,34 @@
 export default function LoginPage() {
-  return <div>LOGIN PAGE</div>;
+  return (
+    <div className="flex flex-col items-center">
+      <form className="wax-w-xl py-8 px-4">
+        <div className="mb-4">
+          <label>Email</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            className="border rounded w-full py-2 px-4 border-black"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            className="border rounded w-full py-2 px-4 border-black"
+          />
+        </div>
+
+        <button
+          className="bg-blue-500 rounded-lg py-2 px-4 mt-2 text-white  "
+          type="submit"
+        >
+          Login
+        </button>
+      </form>
+    </div>
+  );
 }
