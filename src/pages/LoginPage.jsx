2a729cd85834a7be9f57b4fdbe7478250e38a3c9
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2025-01-29 13:52:22 +0100

    login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
 import useNumberStore from "../js/store/useNumberStore";
+import { validateEmail } from "../js/errorHandling/validate/validateEmail";
+import { loginUser } from "../js/API/loginFetch";
+import { validatePassword } from "../js/errorHandling/validate/validatePassword";
 
 export default function LoginPage() {
   const [formData, setFormData] = useState({
     email: "",
     password: "",
   });
+  const [errors, setErrors] = useState({});
   const { number, increase, decrease, setNumber } = useNumberStore();
 
   function handleInputChange(event) {
@@ -13,6 +17,38 @@ export default function LoginPage() {
       ...formData,
       [event.target.name]: event.target.value,
     });
+  }
+
+  function validateForm() {
+    const newErrors = {};
+
+    if (!validateEmail(formData.email)) {
+      newErrors.email = "Please use a 'stud.noroff.no' email address.";
+    }
+
+    if (!validatePassword(formData.password)) {
+      newErrors.password = "Password must be at least 8 characters.";
+    }
+
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    if (!validateForm()) {
+      console.log("Validation failed");
+      return;
+    }
+
+    try {
+      const result = await loginUser(formData.email, formData.password);
+      console.log("Login successful!", result);
+    } catch (error) {
+      console.log("Login failed", error.message);
+    }
+
+    console.log("Validation works");
   }
 
   return (
@@ -25,7 +61,7 @@ export default function LoginPage() {
         <button onClick={() => setNumber(100)}>Set to 100</button>
       </div>
       <div className="flex flex-col items-center">
-        <form className="wax-w-xl py-8 px-4">
+        <form className="wax-w-xl py-8 px-4" onSubmit={handleSubmit}>
           <div className="mb-4">
             <label>Email</label>
             <input
@@ -36,8 +72,8 @@ export default function LoginPage() {
               placeholder="Email"
               className="border rounded w-full py-2 px-4 border-black"
             />
+            {errors.email && <div className="text-red-500">{errors.email}</div>}
           </div>
-
           <div className="mb-4">
             <label>Password</label>
             <input
@@ -48,6 +84,9 @@ export default function LoginPage() {
               placeholder="Password"
               className="border rounded w-full py-2 px-4 border-black"
             />
+            {errors.password && (
+              <div className="text-red-500">{errors.password}</div>
+            )}
           </div>
 
           <button
