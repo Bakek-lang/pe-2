213c12105fc5dd395ee61e0a18661383821246cb
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2025-01-29 15:52:49 +0100

    navBar structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,18 @@
 import { NavLink } from "react-router-dom";
+import useAuthStore from "../js/store/useAuthStore";
+import DropdownMenu from "./DropdownMenu";
 
 export default function Navbar() {
+  const { isLoggedIn, user, clearUser } = useAuthStore();
+
+  console.log("user: ", user);
+
   return (
     <nav>
-      <ul className="flex">
-        <li className="p-2">
-          <NavLink to="/">Holidaze</NavLink>
-        </li>
-      </ul>
+      <div className="relative max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <NavLink to="/">Holidaze</NavLink>
+        <DropdownMenu />
+      </div>
     </nav>
   );
 }
