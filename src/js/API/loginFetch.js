2686f1194b64e03a95caba28fe86678482fec48a
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2025-01-29 13:30:19 +0100

    login fetch

---
+++ src/js/API/loginFetch.js
@@ -0,0 +1,18 @@
+import { API_AUTH, API_BASE, API_LOGIN } from "./constants";
+
+export async function loginUser(email, password) {
+  const response = await fetch(API_BASE + API_AUTH + API_LOGIN, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify({ email, password }),
+  });
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.message || "Login failed");
+  }
+
+  return response.json();
+}
