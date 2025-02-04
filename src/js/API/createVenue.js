02c4b8fea5f50ed40f601f37955bb01249eaa437
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2025-02-04 19:46:11 +0100

    create venue api function

---
+++ src/js/API/createVenue.js
@@ -0,0 +1,21 @@
+import { API_BASE, API_HOLIDAZE, API_KEY, API_VENUES } from "./constants";
+
+export async function createVenue(venueData, accessToken) {
+  const response = await fetch(API_BASE + API_HOLIDAZE + API_VENUES, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      "X-Noroff-API-Key": API_KEY,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(venueData),
+  });
+  console.log("Response:", response);
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.message || "Failed to create venue");
+  }
+
+  return response.json();
+}
