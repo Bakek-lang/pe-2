3702a9b6233178cb3f1e762eca176e32d95c1590
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2025-02-07 14:53:47 +0100

    update

--- src/pages/ProtectedUpdateVenuePage.jsx
+++ src/pages/ProtectedUpdateVenuePage.jsx
@@ -1,5 +1,39 @@
+import { useNavigate, useParams } from "react-router-dom";
 import UpdateVenuePage from "./updateVenuePage";
+import useAuthStore from "../js/store/useAuthStore";
+import { useEffect, useState } from "react";
+import { fetchVenueById } from "../js/utils/fetchVenue";
 
 export default function ProtectedUpdateVenuePage() {
-  return <UpdateVenuePage />;
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const { user } = useAuthStore();
+  const [venue, setVenue] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function checkOwnership() {
+      try {
+        const venueData = await fetchVenueById(id);
+        if (venueData && venueData.data.owner.name === user.data.name) {
+          setVenue(venueData.data);
+        } else {
+          navigate("/");
+        }
+      } catch (error) {
+        console.error("Error fetching venue data: ", error);
+        navigate("/");
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    checkOwnership();
+  }, [id, user, navigate]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return <UpdateVenuePage venue={venue} />;
 }
