1cfc5f09a7e57d063ee58f04ee77fd02ce5a39d4
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2025-02-07 16:17:28 +0100

    update venue

--- src/pages/UpdateVenuePage.jsx
+++ src/pages/UpdateVenuePage.jsx
@@ -7,12 +7,11 @@ export default function UpdateVenuePage({ venue }) {
   const { accessToken } = useAuthStore();
   const { addNotification } = useNotificationStore();
 
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
-  const [imageUrl, setImageUrl] = useState("");
-  const [price, setPrice] = useState("");
-  const [maxGuests, setMaxGuests] = useState("");
-  const [rating, setRating] = useState("");
+  const [name, setName] = useState(venue.name);
+  const [description, setDescription] = useState(venue.description);
+  const [imageUrl, setImageUrl] = useState(venue.media[0].url);
+  const [price, setPrice] = useState(venue.price);
+  const [maxGuests, setMaxGuests] = useState(venue.maxGuests);
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -30,12 +29,11 @@ export default function UpdateVenuePage({ venue }) {
       }),
       price: Number(price),
       maxGuests: Number(maxGuests),
-      rating: parseFloat(rating),
     };
 
     try {
       const updatedVenue = await updateVenue(venueData, accessToken, venue.id);
-      console.log("Updated Venue: ", updateVenue);
+      console.log("Updated Venue: ", updatedVenue);
       addNotification("Updated Venue successfully!", "success");
     } catch (error) {
       console.log("Updating venue failed:", error.message);
@@ -47,6 +45,7 @@ export default function UpdateVenuePage({ venue }) {
     <div className="max-w-lg mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Update Venue</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+        <label>Venue Title</label>
         <input
           type="text"
           placeholder="Venue Title"
@@ -56,6 +55,7 @@ export default function UpdateVenuePage({ venue }) {
           className="p-2 border rounded"
         />
 
+        <label>Description</label>
         <textarea
           placeholder="Description"
           value={description}
@@ -64,6 +64,7 @@ export default function UpdateVenuePage({ venue }) {
           className="p-2 border rounded"
         ></textarea>
 
+        <label>Image URL</label>
         <input
           type="text"
           placeholder="Image URL"
@@ -72,6 +73,7 @@ export default function UpdateVenuePage({ venue }) {
           className="p-2 border rounded"
         />
 
+        <label>Price</label>
         <input
           type="number"
           placeholder="Price"
@@ -82,6 +84,7 @@ export default function UpdateVenuePage({ venue }) {
           className="p-2 border rounded"
         />
 
+        <label>Max Guests</label>
         <input
           type="number"
           placeholder="Max Guests"
@@ -92,19 +95,8 @@ export default function UpdateVenuePage({ venue }) {
           className="p-2 border rounded"
         />
 
-        <input
-          type="number"
-          placeholder="Rating (1-5)"
-          value={rating}
-          onChange={(e) => setRating(e.target.value)}
-          required
-          min="0"
-          max="5"
-          className="p-2 border rounded"
-        />
-
         <button type="submit" className="bg-blue-500 text-white p-2 rounded">
-          Create Venue
+          Update Venue
         </button>
       </form>
     </div>
