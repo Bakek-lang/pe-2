fafcd75924c77d5a58aae510fd8d59abb105d6d6
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2025-02-14 23:28:53 +0100

    media gallery set up

--- src/pages/VenuePage.jsx
+++ src/pages/VenuePage.jsx
@@ -57,80 +57,91 @@ export default function VenuePage() {
   }
 
   return (
-    <div className="flex flex-col justify-center p-4">
-      <div className="media-gallery">
-        <div className="h-96 w-1/2 overflow-hidden">
+    <div className="flex flex-col p-4 md:flex-row justify-center  ">
+      <div className="media-gallery flex flex-col ">
+        <div className="w-full h-96 overflow-hidden">
           <img
-            src={mainImage.url}
-            alt={mainImage.alt}
+            src={mainImage?.url}
+            alt={mainImage?.alt}
             className="w-full h-full object-cover rounded-t-lg"
             onError={handleImageError}
           />
         </div>
-        <div className="flex gap-2 mt-4 overflow-x-auto">
-          {venue.media.map((media, index) => (
-            <img
-              key={index}
-              src={media.url}
-              alt={media.alt}
-              onClick={() => setMainImage({ url: media.url, alt: media.alt })}
-              onError={handleImageError}
-              className={`h-20 w-15 object-cover rounded cursor-pointer 
+        <div className="w-full ">
+          <div
+            className={`flex gap-2 mt-4 ${
+              venue.media.length === 4 ? "justify-between" : "overflow-x-auto"
+            }`}
+          >
+            {venue.media.map((media, index) => (
+              <img
+                key={index}
+                src={media.url}
+                alt={media.alt}
+                onClick={() => setMainImage({ url: media.url, alt: media.alt })}
+                onError={handleImageError}
+                className={`object-cover rounded cursor-pointer 
               ${
                 mainImage && media.url === mainImage.url
                   ? "border-4 border-blue-400"
                   : "border border-gray-300"
               } 
+              ${venue.media.length === 4 ? "w-1/4 h-20" : "w-20 h-20"}
               `}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="ml-8">
+        <div className="flex justify-between items-center">
+          <div className="mt-2 flex items-center gap-3">
+            <img
+              src={venue.owner.avatar.url}
+              alt={venue.owner.avatar.url}
+              className="h-8 w-8 rounded-full object-cover"
+              onError={(event) => {
+                event.target.onerror = null;
+                event.target.src = "https://placehold.co/600x400";
+              }}
             />
-          ))}
+            <p className="text-sm">Managed by {venue.owner.name}</p>
+          </div>
+          <div className="flex items-center mt-1 ">
+            <p className="text-lg">{venue.rating}</p>
+            <FaStar className="ml-0.5" />
+          </div>
         </div>
-      </div>
-      <div className="flex justify-between items-center">
-        <div className="mt-2 flex items-center gap-3">
-          <img
-            src={venue.owner.avatar.url}
-            alt={venue.owner.avatar.url}
-            className="h-8 w-8 rounded-full object-cover"
-            onError={(event) => {
-              event.target.onerror = null;
-              event.target.src = "https://placehold.co/600x400";
-            }}
-          />
-          <p className="text-sm">Managed by {venue.owner.name}</p>
-        </div>
-        <div className="flex items-center mt-1 ">
-          <p className="text-lg">{venue.rating}</p>
-          <FaStar className="ml-0.5" />
-        </div>
-      </div>
-      <p className="text-gray-500">
-        {venue.location?.country
-          ? venue.location.city
-            ? `${venue.location.city}, ${venue.location.country}`
-            : venue.location.country
-          : "Location not available"}
-      </p>
+        <p className="text-gray-500">
+          {venue.location?.country
+            ? venue.location.city
+              ? `${venue.location.city}, ${venue.location.country}`
+              : venue.location.country
+            : "Location not available"}
+        </p>
 
-      <h1 className="text-2xl ">{venue.name}</h1>
-      <p className="mt-2">{venue.description}</p>
-      <div className="flex items-center mt-2">
-        <FaUsers size={40} />
-        <p className="ml-1">Max Guests: {venue.maxGuests}</p>
-      </div>
-      <div className="flex items-center">
-        <FaMoneyBillAlt size={40} />
-        <p className="ml-1">{venue.price} NOK per night</p>
-      </div>
-      <div>
-        <div className="flex justify-between mt-4">{renderFeatures(venue)}</div>
-      </div>
-      <div>
-        <BookingCalendar
-          bookings={venue.bookings || []}
-          venueId={venue.id}
-          maxGuests={venue.maxGuests}
-        />
+        <h1 className="text-2xl ">{venue.name}</h1>
+        <p className="mt-2">{venue.description}</p>
+        <div className="flex items-center mt-2">
+          <FaUsers size={40} />
+          <p className="ml-1">Max Guests: {venue.maxGuests}</p>
+        </div>
+        <div className="flex items-center">
+          <FaMoneyBillAlt size={40} />
+          <p className="ml-1">{venue.price} NOK per night</p>
+        </div>
+        <div>
+          <div className="flex justify-between mt-4">
+            {renderFeatures(venue)}
+          </div>
+        </div>
+        <div>
+          <BookingCalendar
+            bookings={venue.bookings || []}
+            venueId={venue.id}
+            maxGuests={venue.maxGuests}
+          />
+        </div>
       </div>
     </div>
   );
