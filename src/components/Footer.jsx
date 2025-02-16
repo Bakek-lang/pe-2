4e5ce16bb5f24747b3bbdf3948548652989bbce1
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2025-02-16 20:08:34 +0100

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-blue-400">
-      <p className="text-center text-white text-lg p-2">&copy;Holidaze</p>
+      <p className="text-center text-white text-lg p-2">&copy;Holidaze 2025</p>
     </footer>
   );
 }
