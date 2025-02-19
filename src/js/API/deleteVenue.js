c7db0295700fb60dfb71cb9f7211365f0b845f56
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2025-02-19 18:51:03 +0100

    deleteVenue jsdocs

--- src/js/API/deleteVenue.js
+++ src/js/API/deleteVenue.js
@@ -1,5 +1,13 @@
 import { API_BASE, API_HOLIDAZE, API_KEY, API_VENUES } from "./constants";
-
+/**
+ * Deletes a venue by sending a DELETE request to the Holidaze API.
+ *
+ * @param {string} venueId - The ID of the venue to be deleted.
+ * @param {string} accessToken - The user's access token for authentication.
+ * @returns {Promise<boolean|Object>} A promise that resolves to `true` if the deletion is successful (status 204),
+ * or the API response if additional data is returned.
+ * @throws {Error} Throws an error if the request fails, with a message from the API response.
+ */
 export async function deleteVenue(venueId, accessToken) {
   const response = await fetch(
     API_BASE + API_HOLIDAZE + API_VENUES + "/" + venueId,
