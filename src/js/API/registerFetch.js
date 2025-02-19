dd3536c6877037e9d01610220f78bbb11ad56950
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2025-02-19 18:52:09 +0100

    registerFetch jsdocs

--- src/js/API/registerFetch.js
+++ src/js/API/registerFetch.js
@@ -1,5 +1,13 @@
 import { API_AUTH, API_BASE, API_REGISTER } from "./constants";
-
+/**
+ * Registers a new user by sending a POST request to the Holidaze API.
+ *
+ * @param {string} name - The name of the user to register.
+ * @param {string} email - The user's email address.
+ * @param {string} password - The user's password.
+ * @returns {Promise<Object>} A promise that resolves to the newly created user data.
+ * @throws {Error} Throws an error if the registration fails, with a message from the API response.
+ */
 export async function registerUser(name, email, password) {
   const response = await fetch(API_BASE + API_AUTH + API_REGISTER, {
     headers: {
