3a700f135a8048d429692dee57afdc12bbfc1845
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2025-02-19 18:51:21 +0100

    fetchBookingsByProfile jsdocs

--- src/js/API/fetchBookingsByProfile.js
+++ src/js/API/fetchBookingsByProfile.js
@@ -5,7 +5,15 @@ import {
   API_KEY,
   API_PROFILES,
 } from "./constants";
-
+/**
+ * Fetches all bookings associated with a given user profile from the Holidaze API.
+ *
+ * @param {Object} user - The user object containing profile details.
+ * @param {string} user.data.name - The username used to fetch bookings.
+ * @param {string} accessToken - The user's access token for authentication.
+ * @returns {Promise<Object[]>} A promise that resolves to an array of booking objects.
+ * If an error occurs, an empty array is returned.
+ */
 export async function fetchBookingsByProfile(user, accessToken) {
   try {
     const response = await fetch(
