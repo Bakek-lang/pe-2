94c8926d6b216eeff200c580701655fa356db5b4
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2025-02-19 18:51:48 +0100

    fetchVenuesbyProfile jsdocs

--- src/js/API/fetchVenuesByProfile.js
+++ src/js/API/fetchVenuesByProfile.js
@@ -5,7 +5,15 @@ import {
   API_PROFILES,
   API_VENUES,
 } from "./constants";
-
+/**
+ * Fetches all venues associated with a given user profile from the Holidaze API.
+ *
+ * @param {Object} user - The user object containing profile details.
+ * @param {string} user.data.name - The username used to fetch venues.
+ * @param {string} accessToken - The user's access token for authentication.
+ * @returns {Promise<Object[]>} A promise that resolves to an array of venue objects.
+ * If an error occurs, an empty array is returned.
+ */
 export async function fetchVenuesByProfile(user, accessToken) {
   try {
     const response = await fetch(
