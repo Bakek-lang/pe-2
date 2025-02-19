d6db83981746273614a6fd0e2ecbbaddef3e7f3b
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2025-02-19 19:00:58 +0100

    errorhandling jsdocs

--- src/js/errorHandling/validate/validatePassword.js
+++ src/js/errorHandling/validate/validatePassword.js
@@ -1,3 +1,11 @@
+/**
+ * Validates if the provided password meets the minimum length requirement.
+ *
+ * The function checks that the password is at least 8 characters long.
+ *
+ * @param {string} password - The password to validate.
+ * @returns {boolean} Returns `true` if the password is at least 8 characters long; otherwise, returns `false`.
+ */
 export function validatePassword(password) {
   return password.length >= 8;
 }
